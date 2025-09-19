b4ef847045fcb0a782a027fc0e5e8329948c3784
FlorianFleisch <florian.fleisch@student.htldornbirn.at>
2025-09-19 11:26:25 +0200

    added_better_unitests

--- Datastructure_and_Algorithmen/Datastrukture_Tests/UnitTest1.cs
+++ Datastructure_and_Algorithmen/Datastrukture_Tests/UnitTest1.cs
@@ -1,17 +1,39 @@
+using System;
+using System.IO;
 using NUnit.Framework;
 using Datastructure;
 
-namespace Datastructure_Tests
+namespace Datastructure.Tests
 {
+    [TestFixture]
     public class SimpleLinkedListTests
     {
         [Test]
-        public void Add_OneElement_ListIsNotEmpty()
+        public void PrintAll_EmptyList_WritesNothing()
         {
             var list = new SimpleLinkedList();
-            list.Add(10);
+            using var sw = new StringWriter();
+            Console.SetOut(sw);
 
-            Assert.Pass("Element wurde hinzugefügt, kein Fehler aufgetreten.");
+            list.PrintAll();
+
+            Assert.That(sw.ToString(), Is.EqualTo(string.Empty));
+        }
+
+        [Test]
+        public void Add_ThenPrintAll_PrintsValuesInInsertionOrder()
+        {
+            var list = new SimpleLinkedList();
+            using var sw = new StringWriter();
+            Console.SetOut(sw);
+
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.PrintAll();
+
+            var expected = $"10{Environment.NewLine}20{Environment.NewLine}30{Environment.NewLine}";
+            Assert.AreEqual(expected, sw.ToString());
         }
     }
 }
