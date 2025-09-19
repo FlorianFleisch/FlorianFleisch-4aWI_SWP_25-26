4f81244c454938d48b0fe07976a32f43ab54d5e5
FlorianFleisch <florian.fleisch@student.htldornbirn.at>
2025-09-19 09:31:31 +0200

    finished simple list

---
+++ Datastructure_and_Algorithmen/Datastructure/SimpleLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using Common;
+
+namespace Datastructure
+{
+    public class SimpleLinkedList
+    {
+        private Node? head;
+
+        public void Add(int value)
+        {
+            Node newNode = new Node(value);
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node current = head;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+        }
+
+        public void PrintAll()
+        {
+            Node? current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data);
+                current = current.next;
+            }
+        }
+    }
+}
